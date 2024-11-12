758b15b9a6d18e28c61e688af088853230946b48
damert123 <damert337@gmail.com>
2024-11-12 12:29:08 +0300

    Поменял ответ

--- app/Http/Controllers/CentrifugaController.php
+++ app/Http/Controllers/CentrifugaController.php
@@ -20,7 +20,6 @@ class CentrifugaController extends Controller
             'params' => [
                 'channel' => $channel,
                 'data' => [
-                    'message' => $message,
                     "value" => $message,
                     "name" => $name,
                     "email" => $email,
@@ -34,7 +33,7 @@ class CentrifugaController extends Controller
             'Authorization' => 'apikey 1a4082eb-1b25-49eb-a2d6-717673e6af84',
         ])->post('http://localhost:8001/api', $data);
 
-        return response()->noContent();
+        return response($data);
 
 
     }
