11ce341189e5c834b501ed96399e6c3a3b5fccc5
damert123 <damert337@gmail.com>
2024-11-12 12:09:50 +0300

    Поменял apikey

--- app/Http/Controllers/CentrifugaController.php
+++ app/Http/Controllers/CentrifugaController.php
@@ -31,7 +31,7 @@ class CentrifugaController extends Controller
         ];
 
         $response = Http::withHeaders([
-            'Authorization' => 'apikey YOUR_API_KEY',
+            'Authorization' => 'apikey 1a4082eb-1b25-49eb-a2d6-717673e6af84',
         ])->post('http://localhost:8001/api', $data);
 
         return response()->noContent();
